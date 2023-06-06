48dd7ecaf86d3a00188f76eb0e1f8ca7925b4590
Ritul Jain <rituljain0708@gmail.com>
2023-06-06 20:30:39 +0530

    adding mongodb database for storing messages

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -1,4 +1,6 @@
-const handler = (req, res) => {
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
   if (req.method === 'POST') {
     const {email, name, message} = req.body;
 
@@ -12,7 +14,29 @@ const handler = (req, res) => {
       name,
       message
     };
-    console.log(newMessage);
+
+    let client;
+
+    try {
+      const client = await MongoClient.connect(
+        'mongodb+srv://rituljain0708:a7oVjiBINQYfKbI1@events.jcxvrzj.mongodb.net/blog-app?retryWrites=true&w=majority'
+      );
+    } catch (error) {
+      res.status(500).json({ message: 'Could not connect to database' });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: 'Storing Message Failed' });
+      return;
+    }
+
+
     res.status(201).jsom({ message: 'Successfully Stored Message', message: newMessage});
   }
 }
